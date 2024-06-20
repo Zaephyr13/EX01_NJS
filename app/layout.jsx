e4cf368b979989ee89fb0afc706c425c88d7ad42
Christian ANTUNA <chriscorps@hotmail.com>
2024-06-20 13:09:56 +0200

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,32 @@
+/** @format */
+
+import '@styles/globals.css'
+
+import Navbar from '@components/Navbar'
+import Provider from '@components/Provider'
+
+export const metadata = {
+    title: 'Absurd Shockers',
+    description: 'Structure Esport Professionnelle.',
+}
+
+const RootLayout = ({ children }) => {
+    return (
+        <html lang='fr'>
+            <body>
+                <Provider>
+                    <div className='main'>
+                        <div className='gradient' />
+                    </div>
+
+                    <main className='app'>
+                        <Navbar />
+                        {children}
+                    </main>
+                </Provider>
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
+/** @format */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+    reactStrictMode: true,
+    experimental: {
+        serverComponentsExternalPackages: ['mongoose'],
+    },
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        }
+        return config
+    },
+}
+
+export default nextConfig
